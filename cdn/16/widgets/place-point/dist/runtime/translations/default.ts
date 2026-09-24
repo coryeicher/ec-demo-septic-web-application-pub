0256b15e73344ad65f7c9ce88f25290e8e43ffe6
Cory Eicher <cory@eichcorp.com>
2026-09-24 13:11:46 -0400

    DHP-170 septic viewer demo

---
+++ cdn/16/widgets/place-point/dist/runtime/translations/default.ts
@@ -0,0 +1,6 @@
+export default {
+  pleaseSelectAMap: 'Please configure a map.',
+  pleaseSelectADataSource: 'Please configure a layer data source.',
+  widgetHeading: 'Place project point at a new location',
+  selectedPointLabelPrefix: 'Selected Project',
+}
